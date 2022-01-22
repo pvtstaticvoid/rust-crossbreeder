a02c33a78913a185d22bc92b8610468def5645d8
Roger Liu <pvtstaticvoid@gmail.com>
2022-01-22 11:40:08 -0500

    Implemented crossbreeding for batches with a base breed.

--- Source Code/main.c
+++ Source Code/main.c
@@ -15,19 +15,21 @@ char GENES[NUM_UNIQ_GENES] = {'G', 'H', 'Y', 'W', 'X'};
 #define NUM_GENE_SLOTS 6
 
 // Max number of iterations.
-#define MAX_ITER 10
+#define MAX_ITER 25
 
 // Maximum breeding multiplicity.
-#define MAX_MULTIPLICITY 8
+#define MAX_MULT 8
 
 // A breed structure.
 typedef struct breed_t {
 
     int num_parents;
-    struct breed_t *parents[MAX_MULTIPLICITY];
+    struct breed_t *parents[MAX_MULT];
     int score;
     int genes[7];
     char genes_str[7];
+    bool has_chance;
+    double chance;
 
 } breed_t;
 
@@ -146,10 +148,25 @@ int get_index(const char *genes) {
 }
 
 /**
+ * Visualize a breed's lineage.
+ */
+void visualize(breed_t *breed, int depth) {
+    for (int i = 0; i < depth; i++) {
+        printf("\t");
+    }
+    printf("%s\n", breed->genes_str);
+    for (int i = 0; i < breed->num_parents; i++) {
+        visualize(breed->parents[i], depth + 1);
+    }
+}
+
+/**
  * Try to add a new breed.
  */
 bool try_add(breed_t* ordered_table[], breed_t* running_table[], int *running_table_len,
-             const int batch[], int mult, char *new_genes) {
+             const int batch[], int mult, char *new_genes,
+             bool has_base, int base_idx,
+             bool has_chance, double chance) {
 
     int ordered_table_idx = get_index(new_genes);
     if (ordered_table[ordered_table_idx]) {
@@ -158,7 +175,15 @@ bool try_add(breed_t* ordered_table[], breed_t* running_table[], int *running_ta
 //            printf("too many reds\n");
     } else {
 
+
+
         breed_t *new_breed = malloc(sizeof(breed_t));
+
+        // fill out chance, if required
+        if (has_chance) {
+            new_breed->has_chance = true;
+            new_breed->chance = chance;
+        }
 
         // fill out parents
         new_breed->num_parents = mult;
@@ -176,12 +201,33 @@ bool try_add(breed_t* ordered_table[], breed_t* running_table[], int *running_ta
         ordered_table[ordered_table_idx] = new_breed;
         running_table[*running_table_len] = new_breed;
         *running_table_len += 1;
-        printf("created a new breed %s with %d parents\n", new_breed->genes_str, mult);
 
-        if (check_good(new_breed->genes_str, "GGGYYY")) {
-            printf("FOUND IT!\n");
-            exit(0);
+        // debugging info
+        printf("Trying to add %s", new_genes);
+        if (has_chance) {
+            printf(" (with chance %f)", chance);
         }
+        printf("\n");
+        if (has_base) {
+            printf("\t%s (base)\n", running_table[batch[base_idx]]->genes_str);
+        }
+        for (int i = 0; i < mult; i++) {
+            if (has_base && i == base_idx) {
+                continue;
+            }
+            printf("\t%s\n", running_table[batch[i]]->genes_str);
+        }
+
+//        if (mult >= 3 && batch[0] != batch[1]) {
+//            printf("nice\n");
+//            exit(0);
+//        }
+
+//        if (check_good(new_breed->genes_str, "GHHWHG")) {
+//            printf("FOUND IT!\n");
+//            visualize(new_breed, 0);
+//            exit(0);
+//        }
 
 
     }
@@ -204,8 +250,14 @@ bool crossbreed(breed_t* ordered_table[], breed_t* running_table[], int *running
     // Aggregate gene weight array.
     int W[NUM_GENE_SLOTS][NUM_UNIQ_GENES] = {0};
 
-    // Add up all gene weights.
+    // Add up all gene weights (skipping the base if there is one).
     for (int batch_idx = 0; batch_idx < mult; batch_idx++) {
+
+        // skip the base clone if there is one
+        if (has_base && batch_idx == base_idx) {
+            continue;
+        }
+
         for (int slot = 0; slot < NUM_GENE_SLOTS; slot++) {
             switch (running_table[batch[batch_idx]]->genes_str[slot]) {
                 case 'G':
@@ -233,36 +285,24 @@ bool crossbreed(breed_t* ordered_table[], breed_t* running_table[], int *running
     bool deterministic = true;
     int argmax2d[NUM_GENE_SLOTS][NUM_UNIQ_GENES] = {0};
     int num_maxima[NUM_GENE_SLOTS] = {0};
-    int maxima[NUM_GENE_SLOTS];
+    int max_wt[NUM_GENE_SLOTS];
 
-    // Loop over each slot, and check if there is a maximum gene weight.
+    // Calculate maximum gene weight(s) for each slot
     for (int slot = 0; slot < NUM_GENE_SLOTS; slot++) {
 
-        // skip the base clone for now, if there is one
-        if (has_base && slot == base_idx) {
-            continue;
-        }
-
-        bool unique_max = true;
-        maxima[slot] = -1;
+        max_wt[slot] = -1;
 
         for (int gene = 0; gene < NUM_UNIQ_GENES; gene++) {
-            if (W[slot][gene] > maxima[slot]) {
-
-                // temp
-                unique_max = true;
+            if (W[slot][gene] > max_wt[slot]) {
 
                 // update maxima
-                maxima[slot] = W[slot][gene];
+                max_wt[slot] = W[slot][gene];
 
                 // reset argmax
                 argmax2d[slot][0] = gene;
                 num_maxima[slot] = 1;
 
-            } else if (W[slot][gene] == maxima[slot]) {
-
-                // temp
-                unique_max = false;
+            } else if (W[slot][gene] == max_wt[slot]) {
 
                 // update argmax
                 argmax2d[slot][num_maxima[slot]] = gene;
@@ -270,28 +310,222 @@ bool crossbreed(breed_t* ordered_table[], breed_t* running_table[], int *running
             }
         }
 
-        if (!unique_max) {
-            deterministic = false;
-            break;
+    }
+
+
+    // number of ambiguous slots (limited to 1)
+    int num_slots_ambig = 0;
+
+    // the chance of this happening
+    double chance = 1.0;
+    bool has_chance = false;
+
+    // the 3 possible new genes (4+ is impossible)
+    char new_genes_0[7];
+    new_genes_0[6] = '\0';
+    char new_genes_1[7];
+    new_genes_1[6] = '\0';
+    char new_genes_2[7];
+    new_genes_2[6] = '\0';
+
+    // now let's handle the base
+    if (has_base) {
+
+        // strncpy
+        strncpy(new_genes_0, running_table[batch[base_idx]]->genes_str, 6);
+        strncpy(new_genes_1, running_table[batch[base_idx]]->genes_str, 6);
+        strncpy(new_genes_2, running_table[batch[base_idx]]->genes_str, 6);
+
+        // Loop over each slot, and check if there is a maximum gene weight.
+        for (int slot = 0; slot < NUM_GENE_SLOTS; slot++) {
+            if (max_wt[slot] > 10) {
+                // there is a double up
+                // use original algorithm
+
+                if (num_maxima[slot] == 1) {
+                    // unique max
+                    new_genes_0[slot] = GENES[argmax2d[slot][0]];
+                    new_genes_1[slot] = GENES[argmax2d[slot][0]];
+                    new_genes_2[slot] = GENES[argmax2d[slot][0]];
+                } else {
+
+                    // multiple maxima...
+
+                    if (W[slot][3] == max_wt[slot] && W[slot][4] == max_wt[slot]) {
+
+                        // both reds
+                        new_genes_0[slot] = 'W';
+                        new_genes_1[slot] = 'X';
+                        new_genes_2[slot] = 'X';
+                        num_slots_ambig += 1;
+                        chance = 0.5;
+                        has_chance = true;
+
+                    } else if (W[slot][3] == max_wt[slot]) {
+                        // W only
+                        new_genes_0[slot] = 'W';
+                        new_genes_1[slot] = 'W';
+                        new_genes_2[slot] = 'W';
+
+                    } else if (W[slot][4] == max_wt[slot]) {
+                        // X only
+                        new_genes_0[slot] = 'X';
+                        new_genes_1[slot] = 'X';
+                        new_genes_2[slot] = 'X';
+                    } else if (W[slot][0] == max_wt[slot] && W[slot][1] == max_wt[slot] && W[slot][2] == max_wt[slot]) {
+                        // all greens
+                        new_genes_0[slot] = 'G';
+                        new_genes_1[slot] = 'H';
+                        new_genes_2[slot] = 'Y';
+                        num_slots_ambig += 1;
+                        chance = 0.33;
+                        has_chance = true;
+
+                    } else if (W[slot][0] == max_wt[slot] && W[slot][1] == max_wt[slot]) {
+                        // GH_
+                        new_genes_0[slot] = 'G';
+                        new_genes_1[slot] = 'H';
+                        new_genes_2[slot] = 'H';
+                        num_slots_ambig += 1;
+                        chance = 0.5;
+                        has_chance = true;
+                    } else if (W[slot][0] == max_wt[slot] && W[slot][2] == max_wt[slot]) {
+                        // G_Y
+                        new_genes_0[slot] = 'G';
+                        new_genes_1[slot] = 'Y';
+                        new_genes_2[slot] = 'Y';
+                        num_slots_ambig += 1;
+                        chance = 0.5;
+                        has_chance = true;
+                    } else if (W[slot][1] == max_wt[slot] && W[slot][2] == max_wt[slot]) {
+                        // _HY
+                        new_genes_0[slot] = 'H';
+                        new_genes_1[slot] = 'Y';
+                        new_genes_2[slot] = 'Y';
+                        num_slots_ambig += 1;
+                        chance = 0.5;
+                        has_chance = true;
+                    } else if (W[slot][0] == max_wt[slot]) {
+                        // G only
+                        new_genes_0[slot] = 'G';
+                        new_genes_1[slot] = 'G';
+                        new_genes_2[slot] = 'G';
+                    } else if (W[slot][1] == max_wt[slot]) {
+                        // H only
+                        new_genes_0[slot] = 'H';
+                        new_genes_1[slot] = 'H';
+                        new_genes_2[slot] = 'H';
+                    } else if (W[slot][2] == max_wt[slot]) {
+                        // Y only
+                        new_genes_0[slot] = 'Y';
+                        new_genes_1[slot] = 'Y';
+                        new_genes_2[slot] = 'Y';
+                    } else {
+                        printf("fuck\n");
+                        exit(1);
+                    }
+
+                }
+
+
+
+            } else {
+                // there is not a double up, more work needs to be done...
+
+                if (W[slot][3] > 0 && W[slot][4] > 0) {
+
+                    // W & X
+                    switch (running_table[batch[base_idx]]->genes_str[slot]) {
+                        case 'G':
+                        case 'H':
+                        case 'Y':
+                            // slot overwritten to W/X
+                            new_genes_0[slot] = 'W';
+                            new_genes_1[slot] = 'X';
+                            new_genes_2[slot] = 'X';
+                            num_slots_ambig += 1;
+                            chance = 0.5;
+                            has_chance = true;
+                            break;
+                        case 'W':
+                        case 'X':
+                            // slot unaffected
+                            break;
+                        default:
+                            fprintf(stderr, "Invalid gene.");
+                            exit(1);
+                    }
+
+                } else if (W[slot][3] > 0) {
+                    // just W
+                    switch (running_table[batch[base_idx]]->genes_str[slot]) {
+                        case 'G':
+                        case 'H':
+                        case 'Y':
+                            // slot overwritten to W
+                            new_genes_0[slot] = 'W';
+                            new_genes_1[slot] = 'W';
+                            new_genes_2[slot] = 'W';
+                            break;
+                        case 'W':
+                        case 'X':
+                            // slot unaffected
+                            break;
+                        default:
+                            fprintf(stderr, "Invalid gene.");
+                            exit(1);
+                    }
+                } else if (W[slot][4] > 0) {
+                    // just X
+                    switch (running_table[batch[base_idx]]->genes_str[slot]) {
+                        case 'G':
+                        case 'H':
+                        case 'Y':
+                            // slot overwritten to W
+                            new_genes_0[slot] = 'X';
+                            new_genes_1[slot] = 'X';
+                            new_genes_2[slot] = 'X';
+                            break;
+                        case 'W':
+                        case 'X':
+                            // slot unaffected
+                            break;
+                        default:
+                            fprintf(stderr, "Invalid gene.");
+                            exit(1);
+                    }
+                } else {
+                    // neither X or Y
+                    // slot unaffected
+                }
+
+            }
         }
 
     }
 
-    char new_genes[7];
-    new_genes[6] = '\0';
+
 
     // Debugging information.
-    if (deterministic) {
+    if (deterministic && !has_base) {
 //        printf("Deterministic!\t");
-        for (int w = 0; w < NUM_GENE_SLOTS; w++) {
-            new_genes[w] = GENES[argmax2d[w][0]];
-        }
+//        for (int w = 0; w < NUM_GENE_SLOTS; w++) {
+//            new_genes_0[w] = GENES[argmax2d[w][0]];
+//        }
 //        printf("%s\n", new_genes);
 
-        try_add(ordered_table, running_table, running_table_len, batch, mult, new_genes);
+        try_add(ordered_table, running_table, running_table_len, batch, mult, new_genes_0, false, -1, false, chance);
 
-    } else {
-//        printf("Ambiguous!\n");
+    } else if (has_base) {
+
+        // only do shit if at most 1 ambiguous slot
+        if (num_slots_ambig <= 1) {
+            try_add(ordered_table, running_table, running_table_len, batch, mult, new_genes_0, true, base_idx, has_chance, chance);
+            try_add(ordered_table, running_table, running_table_len, batch, mult, new_genes_1, true, base_idx, has_chance, chance);
+            try_add(ordered_table, running_table, running_table_len, batch, mult, new_genes_2, true, base_idx, has_chance, chance);
+        }
+
+
     }
 
     return deterministic;
@@ -312,7 +546,7 @@ int main() {
     int running_table_curr = 0;
 
     // Open data file.
-    FILE *file = fopen("../Data/Set 5.txt", "r");
+    FILE *file = fopen("../Data/Set 1.txt", "r");
     if (!file) {
         perror("fopen");
         exit(1);
@@ -338,7 +572,7 @@ int main() {
 
     // Print data file summary.
     printf("Number of starter seeds: %d.\n", running_table_len);
-    assert(running_table_len >= MAX_MULTIPLICITY);
+    assert(running_table_len >= MAX_MULT);
 
     // Main loop.
 
@@ -347,7 +581,9 @@ int main() {
 
         printf("\n\n\nIteration %d.\n", iter);
 
-        for (int mult_curr_iter = 1; mult_curr_iter <= MAX_MULTIPLICITY; mult_curr_iter++) {
+        for (int mult_curr_iter = 1; mult_curr_iter <= MAX_MULT; mult_curr_iter++) {
+
+
 
             // Temporary.
 //            if (mult_curr_iter != 3) {
@@ -363,14 +599,14 @@ int main() {
             while (true) {
 
                 // Crossbreed current batch of genes.
-                crossbreed(ordered_table, running_table, &running_table_len, batch, mult_curr_iter, false, -1);
+//                crossbreed(ordered_table, running_table, &running_table_len, batch, mult_curr_iter, false, -1);
 
-//                // base plant might matter
-//                if (2 <= mult_curr_iter && mult_curr_iter <= 5) {
-//                    for (int base = 0; base < mult_curr_iter; base++) {
-//                        crossbreed(ordered_table, running_table, &running_table_len, batch, mult_curr_iter, true, base);
-//                    }
-//                }
+                // base plant might matter
+                if (2 <= mult_curr_iter && mult_curr_iter <= 5) {
+                    for (int base = 0; base < mult_curr_iter; base++) {
+                        crossbreed(ordered_table, running_table, &running_table_len, batch, mult_curr_iter, true, base);
+                    }
+                }
 
                 // Loop variables.
                 int batch_idx = 0;
